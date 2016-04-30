e79cc16bb7d0a376d76425ab4f6645a9cf042fb5
Prometheus <arvindkumarrukmaji@gmail.com>
2016-04-30 21:18:32 +0000

    Some express code, npm start script

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,17 @@
-var aloha = "Aloha Variables";
-console.log(aloha);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+app.get("/", function(req, res){
+   res.send("Hello from Express Routes!"); 
+});
+
+app.get("/routing", function(req, res) {
+    res.send("Hello! This is Express Routing!");
+});
+
+app.listen(port, function(err) {
+    if(err) console.log("Error:", err);
+    else console.log("The server is running on port:", port);
+})
