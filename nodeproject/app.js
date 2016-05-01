b7807a55f09ed495d4e0a1b69159d2d7a503c297
Prometheus <arvindkumarrukmaji@gmail.com>
2016-05-01 17:03:22 +0000

    Some ejs

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,14 +4,20 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
-
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-   res.send("Hello from Express Routes!"); 
+   /*res.send("Hello from Express Routes!"); */
+   res.render('index', 
+                {
+                    list: ['1st Value', '2nd Value', '3rd Value'],
+                    nav: ['Services', 'Portfolio', 'About', 'Team', 'Contact', 'NewThing']
+                }
+            );
 });
 
 app.get("/routing", function(req, res) {
